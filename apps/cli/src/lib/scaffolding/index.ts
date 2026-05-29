bbc1f70ea4c456f5e51ead5e62784b80506b9678
AIO <Sebastian-Stepper@gmx.de>
2026-05-29 14:42:08 +0200

    feat: add init review step and consistent run phase progress

--- apps/cli/src/lib/scaffolding/index.ts
+++ apps/cli/src/lib/scaffolding/index.ts
@@ -1,3 +1,3 @@
 export { addToConfigFile } from './config-file'
-export { ScaffoldError, scaffoldProject } from './scaffold'
+export { ScaffoldError, scaffoldProject, type WrittenFile } from './scaffold'
 export { CONFIG_FILE_NAME, type SecretsDest } from './values'
