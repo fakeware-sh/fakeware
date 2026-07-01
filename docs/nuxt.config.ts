9002aaeed3e720aa9de9d95ba124abe5bf39ccf7
Sebastian Stepper <Sebastian-Stepper@gmx.de>
2026-07-01 21:16:44 +0200

    docs: add docs Dockerfile, CI workflow, and Nuxt config

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -3,6 +3,11 @@ export default defineNuxtConfig({
   site: {
     name: 'Fakeware',
   },
+  content: {
+    database: {
+      type: 'nodesqlite',
+    },
+  },
   app: {
     head: {
       link: [{ rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicon-96x96.png' }],
